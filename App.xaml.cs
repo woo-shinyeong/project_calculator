4e1f541bb181d04599ac84794010a19d38316eb1
woo-shinyeong <sywoo@ati2000.co.kr>
2025-08-21 09:53:54 +0900

    초기 커밋.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace project_calculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
